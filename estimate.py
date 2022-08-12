3d5b5db90888b49c868c9502b24139289f0dfd9e
Holim Lim <holim0711@outlook.com>
2022-08-13 02:12:12 +0900

    just estimate

--- estimate.py
+++ estimate.py
@@ -6,27 +6,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import product
 
-import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint, LearningRateMonitor
 from pytorch_lightning.utilities.seed import seed_everything
 from torchmetrics import Accuracy
 
-from weaver.models import get_model
+from weaver.models import get_classifier
 from weaver.optimizers import get_optim
 from weaver.schedulers import get_sched
-from weaver.transforms import get_trfms
+from weaver.transforms import get_xform
 
-from deficient_cifar import NoisyCIFAR10, NoisyCIFAR100
-from deficient_cifar.utils import *
-
-DataModule = {
-    'cifar10': NoisyCIFAR10,
-    'cifar100': NoisyCIFAR100,
-}
+from synthetic_noisy_datasets import NoisyCIFAR10, NoisyCIFAR100
 
 
 def plot_confusion_matrix(cm, labs=None, ylabs=None, xlabs=None, cmap='Blues'):
@@ -70,60 +62,21 @@ def plot_confusion_matrix(cm, labs=None, ylabs=None, xlabs=None, cmap='Blues'):
     return fig
 
 
-class AveragedModelWithBuffers(torch.optim.swa_utils.AveragedModel):
-    def update_parameters(self, model):
-        super().update_parameters(model)
-        for a, b in zip(self.module.buffers(), model.buffers()):
-            a.copy_(b.to(a.device))
-
-
-def change_bn(model, momentum):
-    if isinstance(model, nn.BatchNorm2d):
-        model.momentum = 1 - momentum
-    else:
-        for children in model.children():
-            change_bn(children, momentum)
-
-
 class NoiseEstimator(pl.LightningModule):
 
     def __init__(self, **kwargs):
         super().__init__()
         self.save_hyperparameters()
-
-        self.model = get_model(**self.hparams.model['backbone'])
-        change_bn(self.model, self.hparams.model['momentum'])
+        self.model = get_classifier(**self.hparams.model['backbone'])
         self.criterion = nn.CrossEntropyLoss()
         self.accuracy = nn.ModuleDict({k: Accuracy() for k in ['trn', 'val']})
-
-        if self.hparams.model['ema']:
-            def avg_fn(averaged_model_parameter, model_parameter, _):
-                α = self.hparams.model['momentum']
-                return α * averaged_model_parameter + (1 - α) * model_parameter
-            self.ema = AveragedModelWithBuffers(self.model, avg_fn=avg_fn)
-
-        self.best_err = 1.0
-        self.is_best_err = False
-
-        self.real_T = torch.tensor(transition_matrix(
-            self.hparams.model['backbone']['num_classes'],
-            self.hparams['dataset']['noise_type'],
-            self.hparams['dataset']['noise_ratio']))
 
     @property
     def phase_name(self):
         return 'trn' if self.training else 'val'
 
-    def optimizer_step(self, *args, **kwargs):
-        super().optimizer_step(*args, **kwargs)
-        if self.hparams.model['ema']:
-            self.ema.update_parameters(self.model)
-
     def forward(self, x):
-        if self.training or not self.hparams.model['ema']:
-            return self.model(x)
-        else:
-            return self.ema(x)
+        return self.model(x)
 
     def shared_step(self, batch):
         x, y = batch
@@ -135,10 +88,9 @@ class NoiseEstimator(pl.LightningModule):
 
     def shared_epoch_end(self, outputs):
         phase = self.phase_name
-        err = 1 - self.accuracy[phase].compute()
-        self.log(f'{phase}/err', err)
+        acc = self.accuracy[phase].compute()
+        self.log(f'{phase}/acc', acc)
         self.accuracy[phase].reset()
-        return err
 
     def training_step(self, batch, batch_idx):
         return self.shared_step(batch)
@@ -146,220 +98,81 @@ class NoiseEstimator(pl.LightningModule):
     def training_epoch_end(self, outputs):
         self.shared_epoch_end(outputs)
 
-    def validation_step(self, batch, batch_idx, dataloader_idx):
-        if dataloader_idx == 0:
-            return self.shared_step(batch)
-        elif dataloader_idx == 1:
-            return self.test_step(batch, batch_idx)
+    def validation_step(self, batch, batch_idx):
+        return self.shared_step(batch)
 
     def validation_epoch_end(self, outputs):
-        err = self.shared_epoch_end(outputs[0])
-        self.is_best_err = (err < self.best_err)
-        if self.is_best_err:
-            self.best_err = err
-        self.test_epoch_end(outputs[1])
-
-    def test_step(self, batch, batch_idx):
-        x, y = batch
-        ỹ = self(x).softmax(dim=-1)
-        return {'real': y, 'pred': ỹ}
-
-    def test_epoch_end(self, outputs):
-        C = self.hparams.model['backbone']['num_classes']
-        E = self.current_epoch
-        real = torch.concat([x['real'].cpu() for x in outputs])
-        pred = torch.concat([x['pred'].cpu() for x in outputs])
-        conf = pred.max(dim=1).values
-        info = -pred.xlogy(pred).sum(dim=1)
-
-        if E == 0:
-            self.avg_conf = conf
-        else:
-            self.avg_conf = (self.avg_conf * E + conf) / (E + 1)
-
-        if E == 0:
-            self.avg_info = info
-        else:
-            self.avg_info = (self.avg_info * E + info) / (E + 1)
-
-        # just average
-        T1 = torch.zeros((C, C))
-        for y, ỹ in zip(real, pred):
-            T1[y] += ỹ
-        T1 /= T1.sum(axis=1, keepdim=True)
-
-        # max confidence
-        T2 = torch.zeros((C, C))
-        max_conf = torch.zeros(C)
-        for y, ỹ, c in zip(real, pred, conf):
-            if max_conf[y] < c:
-                max_conf[y] = c
-                T2[y] = ỹ
-
-        # min entropy
-        T3 = torch.zeros((C, C))
-        min_info = torch.ones(C) * torch.tensor(C).log()
-        for y, ỹ, h in zip(real, pred, info):
-            if min_info[y] > h:
-                min_info[y] = h
-                T3[y] = ỹ
-
-        # max average confidence
-        T4 = torch.zeros((C, C))
-        max_avg_conf = torch.zeros(C)
-        for y, ỹ, c in zip(real, pred, self.avg_conf):
-            if max_avg_conf[y] < c:
-                max_avg_conf[y] = c
-                T4[y] = ỹ
-
-        # min average entropy
-        T5 = torch.zeros((C, C))
-        min_avg_info = torch.ones(C) * torch.tensor(C).log()
-        for y, ỹ, h in zip(real, pred, self.avg_info):
-            if min_avg_info[y] > h:
-                min_avg_info[y] = h
-                T5[y] = ỹ
-
-        # average weighted by average confidence
-        T6 = torch.zeros((C, C))
-        for y, ỹ, c in zip(real, pred, self.avg_conf):
-            T6[y] += ỹ * c
-        T6 /= T6.sum(axis=1, keepdim=True)
-
-        # average weighted by e^(-average entropy)
-        T7 = torch.zeros((C, C))
-        for y, ỹ, h in zip(real, pred, self.avg_info):
-            T7[y] += ỹ * (-h).exp()
-        T7 /= T7.sum(axis=1, keepdim=True)
-
-        self.logger.experiment.add_image('T1', T1, self.current_epoch, dataformats='HW')
-        self.logger.experiment.add_image('T2', T2, self.current_epoch, dataformats='HW')
-        self.logger.experiment.add_image('T3', T3, self.current_epoch, dataformats='HW')
-        self.logger.experiment.add_image('T4', T4, self.current_epoch, dataformats='HW')
-        self.logger.experiment.add_image('T5', T5, self.current_epoch, dataformats='HW')
-        self.logger.experiment.add_image('T6', T6, self.current_epoch, dataformats='HW')
-        self.logger.experiment.add_image('T7', T7, self.current_epoch, dataformats='HW')
-        # self.logger.experiment.add_figure(
-        #     'T1', plot_confusion_matrix(T1), self.current_epoch)
-        # self.logger.experiment.add_figure(
-        #     'T2', plot_confusion_matrix(T2), self.current_epoch)
-        # self.logger.experiment.add_figure(
-        #     'T3', plot_confusion_matrix(T3), self.current_epoch)
-        # self.logger.experiment.add_figure(
-        #     'T4', plot_confusion_matrix(T4), self.current_epoch)
-        # self.logger.experiment.add_figure(
-        #     'T5', plot_confusion_matrix(T5), self.current_epoch)
-        # self.logger.experiment.add_figure(
-        #     'T6', plot_confusion_matrix(T6), self.current_epoch)
-        # self.logger.experiment.add_figure(
-        #     'T7', plot_confusion_matrix(T7), self.current_epoch)
-
-        if self.is_best_err:
-            save_path = os.path.join(self.logger.log_dir, 'T')
-            if not os.path.isdir(save_path):
-                os.makedirs(save_path)
-            torch.save(T1, os.path.join(save_path, f'T1.pth'))
-            torch.save(T2, os.path.join(save_path, f'T2.pth'))
-            torch.save(T3, os.path.join(save_path, f'T3.pth'))
-            torch.save(T4, os.path.join(save_path, f'T4.pth'))
-            torch.save(T5, os.path.join(save_path, f'T5.pth'))
-            torch.save(T6, os.path.join(save_path, f'T6.pth'))
-            torch.save(T6, os.path.join(save_path, f'T7.pth'))
-
-        self.log('val/rmse/T1', ((self.real_T - T1) ** 2).mean().sqrt())
-        self.log('val/rmse/T2', ((self.real_T - T2) ** 2).mean().sqrt())
-        self.log('val/rmse/T3', ((self.real_T - T3) ** 2).mean().sqrt())
-        self.log('val/rmse/T4', ((self.real_T - T4) ** 2).mean().sqrt())
-        self.log('val/rmse/T5', ((self.real_T - T5) ** 2).mean().sqrt())
-        self.log('val/rmse/T6', ((self.real_T - T6) ** 2).mean().sqrt())
-        self.log('val/rmse/T7', ((self.real_T - T7) ** 2).mean().sqrt())
+        self.shared_epoch_end(outputs[0])
 
     def configure_optimizers(self):
         optim = get_optim(self, **self.hparams.optimizer)
         sched = get_sched(optim, **self.hparams.scheduler)
-        sched.extend(self.hparams.steps_per_epoch)
-        return {'optimizer': optim,
-                'lr_scheduler': {'scheduler': sched, 'interval': 'step'}}
+        return {'optimizer': optim, 'lr_scheduler': {'scheduler': sched}}
 
 
-def train(dm, hparams, args):
-    hparams['dataset']['num_clean'] = args.num_clean
-    hparams['dataset']['noise_type'] = args.noise_type
-    hparams['dataset']['noise_ratio'] = args.noise_ratio
-    hparams['dataset']['random_seed'] = args.random_seed
-
-    dm.prepare_data()
-    dm.setup()
-
-    noisy_data = dm.datasets['noisy']
-    noisy_train, noisy_val = torch.utils.data.random_split(
-        noisy_data,
-        [len(noisy_data) - len(noisy_data) // 10, len(noisy_data) // 10])
-    train_loader = DataLoader(
-        noisy_train,
-        batch_size=hparams['dataset']['batch_sizes']['train'],
-        shuffle=True,
-        num_workers=os.cpu_count(),
-        pin_memory=True
-    )
-    val_loader = DataLoader(
-        noisy_val,
-        batch_size=hparams['dataset']['batch_sizes']['val'],
-        num_workers=os.cpu_count(),
-        pin_memory=True
-    )
-    clean_loader = DataLoader(
-        dm.datasets['clean'].datasets[0],
-        batch_size=hparams['dataset']['batch_sizes']['val'],
-        num_workers=os.cpu_count(), pin_memory=True)
+def train(args):
+    config = args.config
+    seed_everything(config['random_seed'])
 
     trainer = pl.Trainer.from_argparse_args(
         args,
-        logger=TensorBoardLogger(
-            f"lightning_logs/matrix/{hparams['dataset']['name']}",
-            f"{args.num_clean}-{args.noise_type}-{args.noise_ratio}-{args.random_seed}"),
+        logger=TensorBoardLogger('logs/estimate', config['dataset']['name']),
         callbacks=[
-            ModelCheckpoint(monitor='val/err', save_top_k=1, save_last=True),
+            ModelCheckpoint(save_last=True,
+                            save_top_k=1, monitor='val/acc', mode='max'),
             LearningRateMonitor(),
         ]
     )
 
-    pl_module = NoiseEstimator(**hparams, steps_per_epoch=len(train_loader))
+    xform_train = get_xform('Compose', transforms=config['transform']['train'])
+    xform_val = get_xform('Compose', transforms=config['transform']['val'])
 
-    trainer.fit(pl_module, train_loader, [val_loader, clean_loader])
+    Dataset = {
+        'CIFAR10': NoisyCIFAR10,
+        'CIFAR100': NoisyCIFAR100,
+    }[config['dataset']['name']]
+
+    dm = Dataset(
+        os.path.join('data', config['dataset']['name']),
+        config['dataset']['num_clean'],
+        config['dataset']['noise_type'],
+        config['dataset']['noise_ratio'],
+        transforms={
+            'noisy': xform_train,
+            'val': xform_val,
+        },
+        batch_sizes={
+            'clean': 0,
+            'noisy': config['dataset']['batch_sizes']['train'],
+            'val': config['dataset']['batch_sizes']['val'],
+        },
+        random_seed=config['dataset']['random_seed'],
+    )
+
+    pl_module = NoiseEstimator(**config)
+
+    trainer.fit(pl_module, dm)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('config', type=str)
-    parser.add_argument('--num_clean', type=int, default=100)
-    parser.add_argument('--noise_type', type=str, default='symmetric')
-    parser.add_argument('--noise_ratio', type=float, default=0.2)
-    parser.add_argument('--random_seed', type=int, default=0)
+    parser.add_argument('config', type=lambda x: json.load(open(x)))
+    parser.add_argument('--dataset.num_clean', type=int)
+    parser.add_argument('--dataset.noise_type', type=str)
+    parser.add_argument('--dataset.noise_ratio', type=float)
+    parser.add_argument('--dataset.random_seed', type=int)
+    parser.add_argument('--random_seed', type=int)
     parser = pl.Trainer.add_argparse_args(parser)
 
     args = parser.parse_args()
-    seed_everything(args.random_seed)
-
-    with open(args.config) as file:
-        hparams = json.load(file)
-
-    dm = DataModule[hparams['dataset']['name']](
-        root=os.path.join('data', hparams['dataset']['name']),
-        num_clean=args.num_clean,
-        noise_type=args.noise_type,
-        noise_ratio=args.noise_ratio,
-        transforms={
-            'noisy': get_trfms(hparams['transforms']['train']),
-            'val': get_trfms(hparams['transforms']['val']),
-        },
-        batch_sizes={
-            'clean': 0,
-            'noisy': hparams['dataset']['batch_sizes']['train'],
-            'val': hparams['dataset']['batch_sizes']['val'],
-        },
-        random_seed=args.random_seed,
-        pure_unproved=True,
-    )
-
-    train(dm, hparams, args)
+    if (v := getattr(args, 'dataset.num_clean')) is not None:
+        args.config['dataset']['num_clean'] = v
+    if (v := getattr(args, 'dataset.noise_type')) is not None:
+        args.config['dataset']['noise_type'] = v
+    if (v := getattr(args, 'dataset.noise_ratio')) is not None:
+        args.config['dataset']['noise_ratio'] = v
+    if (v := getattr(args, 'dataset.random_seed')) is not None:
+        args.config['dataset']['random_seed'] = v
+    if (v := getattr(args, 'random_seed')) is not None:
+        args.config['random_seed'] = v
+    train(args)
